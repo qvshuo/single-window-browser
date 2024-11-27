d941a9542184f031e81331ef9a335f46fb012a99
神子撅我撅我 <qvshuo@foxmail.com>
2024-11-27 16:57:53 +0800

    Update browser.c

--- browser.c
+++ browser.c
@@ -1,30 +1,78 @@
+/*
+ * A simple browser based on GTK4 and WebKitGTK 6.0
+ * Usage: browser <URL>
+ */
+
 #include <gtk/gtk.h>
 #include <webkit/webkit.h>
 
-static void activate(GtkApplication *app, gpointer user_data) {
+/* Window activate callback function */
+static void activate(GtkApplication *app, gpointer user_data) 
+{
+    const char *url = (const char *)user_data;
+
+    /* Create main window */
     GtkWidget *window = gtk_application_window_new(app);
+    if (!window) {
+        g_critical("Failed to create window");
+        return;
+    }
+
+    /* Configure window properties */
     gtk_window_set_title(GTK_WINDOW(window), "One Window Browser");
     gtk_window_set_default_size(GTK_WINDOW(window), 1024, 768);
 
+    /* Create WebKit view */
     WebKitWebView *web = WEBKIT_WEB_VIEW(webkit_web_view_new());
+    if (!web) {
+        g_critical("Failed to create web view");
+        return;
+    }
+
+    /* Set WebKit view as window's child */
     gtk_window_set_child(GTK_WINDOW(window), GTK_WIDGET(web));
 
-    gchar *url = user_data;
+    /* Load URL */
     webkit_web_view_load_uri(web, url);
+
+    /* Set focus to WebKit view */
     gtk_widget_grab_focus(GTK_WIDGET(web));
 
+    /* Show window */
     gtk_window_present(GTK_WINDOW(window));
 }
 
-int main(int argc,char **argv) {
-    gchar *url = "https://kernel.org";
-    if(argc == 2)
-        url = argv[1];
+int main(int argc, char **argv) 
+{
+    /* Check command line arguments */
+    if (argc != 2) {
+        g_printerr("Usage: %s <URL>\n", argv[0]);
+        g_printerr("Example: %s https://example.com\n", argv[0]);
+        return 1;
+    }
 
-    GtkApplication *app = gtk_application_new("org.gtk.one-window-browser", G_APPLICATION_DEFAULT_FLAGS);
-    g_signal_connect(app, "activate", G_CALLBACK(activate), url);
+    /* Validate URL format */
+    if (!g_str_has_prefix(argv[1], "http://") && 
+        !g_str_has_prefix(argv[1], "https://")) {
+        g_printerr("Error: URL must start with 'http://' or 'https://'\n");
+        return 1;
+    }
+
+    /* Create application instance */
+    GtkApplication *app = gtk_application_new("org.gtk.one-window-browser",
+                                            G_APPLICATION_DEFAULT_FLAGS);
+    if (!app) {
+        g_critical("Failed to create application");
+        return 1;
+    }
+
+    /* Connect activate signal */
+    g_signal_connect(app, "activate", G_CALLBACK(activate), argv[1]);
+
+    /* Run application */
     int status = g_application_run(G_APPLICATION(app), 0, NULL);
 
+    /* Cleanup */
     g_object_unref(app);
 
     return status;
