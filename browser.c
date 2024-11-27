cfb353c65cef05101b1a049077ea69794d7db701
神子撅我撅我 <qvshuo@foxmail.com>
2024-11-27 17:16:00 +0800

    Update browser.c

--- browser.c
+++ browser.c
@@ -1,15 +1,25 @@
 /*
  * A simple browser based on GTK4 and WebKitGTK 6.0
- * Usage: browser <URL>
+ * Usage: browser --url <URL> [--title <title>] [--size <width>x<height>]
+ * Example: browser --url https://example.com --title "Single-window Browser" --size 1200x800
  */
 
 #include <gtk/gtk.h>
 #include <webkit/webkit.h>
+#include <string.h>
+
+/* Application settings structure */
+typedef struct {
+    const char *url;
+    const char *title;
+    int width;
+    int height;
+} AppSettings;
 
 /* Window activate callback function */
 static void activate(GtkApplication *app, gpointer user_data) 
 {
-    const char *url = (const char *)user_data;
+    AppSettings *settings = (AppSettings *)user_data;
 
     /* Create main window */
     GtkWidget *window = gtk_application_window_new(app);
@@ -19,8 +29,10 @@ static void activate(GtkApplication *app, gpointer user_data)
     }
 
     /* Configure window properties */
-    gtk_window_set_title(GTK_WINDOW(window), "One Window Browser");
-    gtk_window_set_default_size(GTK_WINDOW(window), 1024, 768);
+    gtk_window_set_title(GTK_WINDOW(window), 
+                        settings->title ? settings->title : "Single-window Browser");
+    gtk_window_set_default_size(GTK_WINDOW(window), 
+                               settings->width, settings->height);
 
     /* Create WebKit view */
     WebKitWebView *web = WEBKIT_WEB_VIEW(webkit_web_view_new());
@@ -33,7 +45,7 @@ static void activate(GtkApplication *app, gpointer user_data)
     gtk_window_set_child(GTK_WINDOW(window), GTK_WIDGET(web));
 
     /* Load URL */
-    webkit_web_view_load_uri(web, url);
+    webkit_web_view_load_uri(web, settings->url);
 
     /* Set focus to WebKit view */
     gtk_widget_grab_focus(GTK_WIDGET(web));
@@ -42,18 +54,70 @@ static void activate(GtkApplication *app, gpointer user_data)
     gtk_window_present(GTK_WINDOW(window));
 }
 
+/* Parse window size string (format: widthxheight) */
+static gboolean parse_size(const char *size_str, int *width, int *height)
+{
+    char *endptr;
+    
+    /* Parse width */
+    *width = strtol(size_str, &endptr, 10);
+    if (*endptr != 'x' || *width <= 0) {
+        return FALSE;
+    }
+
+    /* Parse height */
+    *height = strtol(endptr + 1, &endptr, 10);
+    if (*endptr != '\0' || *height <= 0) {
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+/* Print usage information */
+static void print_usage(const char *program_name)
+{
+    g_printerr("Usage: %s --url <URL> [--title <title>] [--size <width>x<height>]\n", 
+               program_name);
+    g_printerr("Example: %s --url https://example.com --title \"Single-window Browser\" --size 1200x800\n", 
+               program_name);
+}
+
 int main(int argc, char **argv) 
 {
-    /* Check command line arguments */
-    if (argc != 2) {
-        g_printerr("Usage: %s <URL>\n", argv[0]);
-        g_printerr("Example: %s https://example.com\n", argv[0]);
+    AppSettings settings = {
+        .url = NULL,
+        .title = NULL,
+        .width = 1200,    /* Default width */
+        .height = 800     /* Default height */
+    };
+
+    /* Parse command line arguments */
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--url") == 0 && i + 1 < argc) {
+            settings.url = argv[++i];
+        } else if (strcmp(argv[i], "--title") == 0 && i + 1 < argc) {
+            settings.title = argv[++i];
+        } else if (strcmp(argv[i], "--size") == 0 && i + 1 < argc) {
+            if (!parse_size(argv[++i], &settings.width, &settings.height)) {
+                g_printerr("Error: Invalid size format. Use <width>x<height>\n");
+                return 1;
+            }
+        } else {
+            print_usage(argv[0]);
+            return 1;
+        }
+    }
+
+    /* Check if URL is provided and valid */
+    if (!settings.url) {
+        g_printerr("Error: URL is required\n");
+        print_usage(argv[0]);
         return 1;
     }
 
-    /* Validate URL format */
-    if (!g_str_has_prefix(argv[1], "http://") && 
-        !g_str_has_prefix(argv[1], "https://")) {
+    if (!g_str_has_prefix(settings.url, "http://") && 
+        !g_str_has_prefix(settings.url, "https://")) {
         g_printerr("Error: URL must start with 'http://' or 'https://'\n");
         return 1;
     }
@@ -67,7 +131,7 @@ int main(int argc, char **argv)
     }
 
     /* Connect activate signal */
-    g_signal_connect(app, "activate", G_CALLBACK(activate), argv[1]);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), &settings);
 
     /* Run application */
     int status = g_application_run(G_APPLICATION(app), 0, NULL);
